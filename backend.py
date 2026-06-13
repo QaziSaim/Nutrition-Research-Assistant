da1814018d414f743f199e89bcfea25edbb9874d
Saim <qazisaim121@gmail.com>
2026-06-14 00:16:10 +0530

    final commit

--- backend.py
+++ backend.py
@@ -3,17 +3,34 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
 
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 load_dotenv()
 
 # Simple ChatOpenAI setup
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
+embedding_model = HuggingFaceEmbeddings(model="sentence-transformers/all-mpnet-base-v2")
+vectorstore = FAISS.load_local(
+    "nutrition_index",
+    embedding_model,
+    allow_dangerous_deserialization=True
+)
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_args = {"k":3}
+)
+
 
 # Prompt Template
 system_prompt = """
-You are a helpful Nutrition Research Assistant.
-Answer questions accurately and professionally based on general nutrition knowledge.
-If you don't know the answer, just say "I don't know".
+You are a helpful Nutrition Research Assistant
+Answer questions accurately and professionally based on general nutrition knowledge
+If you don't know the answer, just say "I don't know"
+"{context}"
 """
 
 prompt = ChatPromptTemplate.from_messages([
@@ -23,8 +40,11 @@ prompt = ChatPromptTemplate.from_messages([
 
 # Chain
 output_parser = StrOutputParser()
-chain = prompt | llm | output_parser
 
+question_answer_chain = create_stuff_documents_chain(llm,prompt) | output_parser
+
+chain = create_retrieval_chain(retriever, question_answer_chain) 
 def ask_question(query: str):
     response = chain.invoke({"input": query})
-    return response
+    return response['answer']
+
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from backend import ask_question
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
 
 app = FastAPI(
     title="Nutrition Research Assistant",
     description="Simple ChatOpenAI Backend",
     version="1.0"
 )
+
+templates = Jinja2Templates(directory="templates")
 
 # CORS - Required for your HTML + JS frontend
 app.add_middleware(
@@ -26,12 +32,13 @@ class QueryResponse(BaseModel):
     answer: str
 
 
-@app.get("/")
-async def root():
-    return {
-        "status": "online",
-        "message": "Nutrition Assistant API is running!"
-    }
+@app.get("/",response_class=HTMLResponse)
+async def root(request:Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html"
+    )
+
 
 
 @app.post("/ask", response_model=QueryResponse)
